e28dfb8f75f42728ee4cf549e45dc3d1fcbce2f9
Алексей Бельченков <u608110@gmail.com>
2018-12-20 18:54:18 +0400

    Update Customer

--- routes/customers.js
+++ routes/customers.js
@@ -47,4 +47,20 @@ module.exports = server => {
            return next(new errors.InternalError(err.message));
        }
     });
+
+    // Update Customer
+    server.put('/customers/:id', async (req, res, next) => {
+        // Check for JSON
+        if (!req.is('application/json')) {
+            return next(new errors.InvalidContentError("Expects 'application/json'"));
+        }
+
+        try {
+            const customer = await Customer.findOneAndUpdate({ _id: req.params.id }, req.body);
+            res.send(200);
+            next();
+        } catch (err) {
+            return next(new errors.ResourceNotFoundError(`There is no customer with the id of ${req.params.id}`));
+        }
+    });
 };
