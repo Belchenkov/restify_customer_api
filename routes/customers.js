c88823c2f4da61257e2268776612da8217b6a826
Алексей Бельченков <u608110@gmail.com>
2018-12-20 18:42:05 +0400

    Add new Customer/Get all customers

--- routes/customers.js
+++ routes/customers.js
@@ -1,8 +1,39 @@
 const errors = require('restify-errors');
+const Customer = require('../models/Customer');
 
 module.exports = server => {
-    server.get('/customers', (req, res, next) => {
-        res.send({ msg: 'test' });
-        next();
+    // Get Customers
+    server.get('/customers', async (req, res, next) => {
+        try {
+            const customers = await Customer.find({});
+            res.send(customers);
+            next();
+        } catch (err) {
+            return next(new errors.InvalidContentError(err));
+        }
+    });
+
+    // Add Customer
+    server.post('/customers', async (req, res, next) => {
+       // Check for JSON
+       if (!req.is('application/json')) {
+           return next(new errors.InvalidContentError("Expects 'application/json'"));
+       }
+
+       const { name, email, balance } = req.body;
+
+       const customer = new Customer({
+           name,
+           email,
+           balance
+       });
+
+       try {
+           const newCustomer = await customer.save();
+           res.send(201);
+           next();
+       } catch (err) {
+           return next(new errors.InternalError(err.message));
+       }
     });
 };
