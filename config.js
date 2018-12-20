3055119497a55b6441509a536808e5eb8db90a24
Алексей Бельченков <u608110@gmail.com>
2018-12-20 18:09:39 +0400

    Connect to MongoDb

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    ENV: process.env.NODE_ENV || 'development',
+    PORT: process.env.PORT || 3000,
+    BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb://belchenkov:12qwasZX@ds139944.mlab.com:39944/restify-customer-api',
+};
