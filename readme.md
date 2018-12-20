5ccb5b3559587f269b9e403dc0854270420c6924
Алексей Бельченков <u608110@gmail.com>
2018-12-20 18:58:45 +0400

    Delete Customer

--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 # Restify Customer API
 
 ### Create a real REST API with database integration and token authentication using Node.js with Restify, Mongoose/MongoDB and JWT.
+
+Restify Node.js Framework: http://restify.com/docs/home/
--- routes/customers.js
+++ routes/customers.js
@@ -63,4 +63,15 @@ module.exports = server => {
             return next(new errors.ResourceNotFoundError(`There is no customer with the id of ${req.params.id}`));
         }
     });
+
+    // Delete Customer
+    server.del('/customers/:id', async (req, res, next) => {
+        try {
+            const customer = await Customer.findOneAndRemove({ _id: req.params.id });
+            res.send(204);
+            next();
+        } catch (err) {
+            return next(new errors.ResourceNotFoundError(`There is no customer with the id of ${req.params.id}`));
+        }
+    });
 };
